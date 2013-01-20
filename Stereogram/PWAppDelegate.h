ba447aa89afd82c0fe34c2ceb380c30f701bc377
Patrick Wallace <paw@unknown-00:1e:c2:bc:25:e9.home>
2013-01-20 13:51:09 +0000

    Initial Commit

---
+++ Stereogram/PWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PWAppDelegate.h
+//  Stereogram
+//
+//  Created by Patrick Wallace on 20/01/2013.
+//  Copyright (c) 2013 Patrick Wallace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Stereogram/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Stereogram
+//
+//  Created by Patrick Wallace on 20/01/2013.
+//  Copyright (c) 2013 Patrick Wallace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PWAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PWAppDelegate class]));
+    }
+}
